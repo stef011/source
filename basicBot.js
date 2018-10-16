5918cdfc8a323adea9691690c2e9a4d0a6037ec6
Zundel Stéphane <zundel.stephane@sfr.fr>
2018-10-16 10:04:12 +0200

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -3025,7 +3025,7 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void(0);
                     else {
-                        $('#meh').click();
+                        $('#Meh').click();
                     }
                 }
             },
